094f329b7ab574d5ff6f9dc275dc2fc4e1658937
Julian Smith <jmsmith118@yahoo.com>
2024-09-12 04:47:31 -0400

    added dynamic schedule

--- src/app/components/Schedule/scheduleCard.tsx
+++ src/app/components/Schedule/scheduleCard.tsx
@@ -55,7 +55,7 @@ const ScheduleCard = () => {
         <div className="border-t border-gray-200">
           <div className="p-4 bg-white rounded-lg md:p-8">
             <div className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
-              {times.map((time, i) => (
+              {times.map((time: any, i) => (
                 <div key={`${time}/${i}`} className="flex flex-col items-center justify-center">
                   <p className="mb-2 text-3xl font-extrabold">{HEADER[i].day}</p>
                   <p className="text-gray-500">{time}</p>
